211b90d11fa782d163d20104405729ebd0c736ad
arijouni <erigeouni2645@gmail.com>
2024-02-07 16:43:56 +0100

    mongoose

---
+++ models/person.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: {
+    type: [String],
+  },
+});
+const person = mongoose.model("person", personSchema);
+export default person;
